029d1931b3502477765f8c202d60836f99592634
calebjames02 <calebjamesbergen@gmail.com>
2024-10-02 11:39:54 -0700

    10/2/24

---
+++ temp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a = 4;
+	int b = 7;
+
+	if(a == 3){
+		printf("hello\n");
+	}else if(b == 8){
+		printf("goodbye\n");
+	}else{
+		printf("whatever\n");
+	}
+
+	return 0;
+}
